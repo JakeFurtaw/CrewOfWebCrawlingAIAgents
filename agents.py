317b7233a14d96953ececb12e60d160307de9ac4
JakeFurtaw <jfurtaw97@gmail.com>
2024-04-29 09:42:33 -0400

    Updated anmes of agents and tasks

--- agents.py
+++ agents.py
@@ -19,48 +19,48 @@ class Agents():
     #     tools = [],
     # )
 
-    Get_example_data = Agent(
+    Data_Loader = Agent(
         name = "Data Analyst",
-        role = "Data Analyst",
-        backstory = """You are an agent that specializes in reading files and directories to get example data that can be used to test the project.""",
+        role = "Data Loader",
         goal="To read files and directories to get example data that can be used to test the project.",
+        backstory = """You are an agent that specializes in reading files and directories to get example data that can be used to test the project.""",
         verbose = True,
         tools = [file_tool, directory_tool],
         allow_delegation = False,
     )
 
-    Question_crawler = Agent(
+    Question_Crawler = Agent(
         name = "Webcrawler",
-        role = "Senior Research Analyst",
+        role = "Research Analyst",
+        goal="To crawl the provided website and come up with questions that students would have about the school.",
         backstory = """You are an agent that specializes in crawling college websites and coming up with questions that new and existing students would 
         have about the school. You are tasked with crawling the website and coming up with questions that new and existing students would have about the school.
         This could include but isnt limited to gerneral information about the school, the programs offered, the campus life, different majors, food offered on campus, building and facility 
         locations, fraternity and sorority information, club information, teacher and faculty information.
         """,
-        goal="To crawl the provided website and come up with questions that students would have about the school.",
         verbose = True,
         tools = [serper_tool, search_tool],
         allow_delegation = False,
     )
 
-    Answer_crawler = Agent(
+    Answer_Crawler = Agent(
         name = "Webcrawler",
-        role = "Senior Research Analyst",
+        role = "Research Analyst",
         backstory = """You are an agent that specializes in crawling college websites and coming up with answers to the questions that the question crawler 
-        agent came up with. Be clear and concise with your answers and make sure to provide as much information as possible.""",
+        agent came up with. Be clear and concise with your answers and make sure to provide as much relevant information as possible.""",
         goal="To crawl the website and come up with answers to the questions that the question crawler agent came up with.",
         verbose = True,
         tools = [serper_tool, search_tool],
         allow_delegation = False,
     )
 
-    Data_formatter = Agent(
+    Data_Formatter = Agent(
         name = "Data Formatter",
         role = "Data Formatter",
+        goal="To format the data that the question and answer crawler agent came up with in a way that is easy to read and understand.",
         backstory = """You are an agent that specializes in formatting data in a way that is easy to read and understand. You are tasked with formatting 
         the data that the question and answer crawler agent came up with in a way that is easy to read and understand for a human. 
         You are to export the data to a text file that can be read by a human.""",
-        goal="To format the data that the question and answer crawler agent came up with in a way that is easy to read and understand.",
         verbose = True,
         tools = [file_tool, directory_tool],
         allow_delegation = False,
--- tasks.py
+++ tasks.py
@@ -1,39 +1,39 @@
 from crewai import Task
-from agents import Question_crawler, Answer_crawler, Get_example_data, Data_formatter, Manager
+from agents import Data_Loader, Question_Crawler, Answer_Crawler, Data_Formatter, Manager
 from main import website_link
 
 # Define the tasks that will be used in the project
-class Tasks():
+class Tasks(website_link):
     Manager = Task(
         description = "Manage the team of agents and provide tasks to each agent that will help further guide the project to sucess.",
         expected_output = "A well managed team and project.",
         agent = Manager
     )
 
-    Get_example_data = Task(
+    Load_Example_Data = Task(
         description = "Read files and directories to get example data that can be used to provide the agents with the kind of data they are looking to complete the project.",
         expected_output = "Example data that can be used to test the project.",
-        agent = Get_example_data
+        agent = Data_Loader
     )
     
-    Question_crawler = Task(
+    Question_Crawler = Task(
         description = """Crawl the provided website {website_link} and all of its sublinks to come up with questions that students would have about the school.
         This could include gerneral information about the school, the programs offered, the campus life, different majors, food offered on campus, building and facility 
         locations, fraternity and sorority information, club information, teacher and faculty information.""",
         expected_output = "A list of questions that students would have about the school.",
-        agent = Question_crawler
+        agent = Question_Crawler
     )
 
-    Answer_crawler = Task(
+    Answer_Crawler = Task(
         description = "Crawl the provided website {website_link} and all of its sublinks to come up with answers to the questions that the question crawler agent came up with.",
         expected_output = "A list of answers to the questions that the question crawler agent came up with.",
-        agent = Answer_crawler
+        agent = Answer_Crawler
     )
 
-    Data_formatter = Task(
+    Data_Formatter = Task(
         description = """Format the data that was collected by the question and answer crawler agents so that it can be used to create a datset to finetune a model.
         Format it so that you have a Question on one line and the answer on the next line.
         """,
         expected_output = "Formatted data that can be used to create a chatbot.",
-        agent = Data_formatter
+        agent = Data_Formatter
     )
